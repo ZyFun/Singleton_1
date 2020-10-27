0956ce0a25cf19d605f14a3eb7da6988986c9497
Dmitriy Danilin <danilindn@icloud.com>
2020-10-27 15:25:06 +0300

    Initial Commit

---
+++ Singleton_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Singleton_1
+//
+//  Created by Дмитрий Данилин on 27.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
